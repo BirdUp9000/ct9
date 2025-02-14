9b728fa355108b03954426927db06836378194a5
Arsenii-Ahamalov <Arsenii-Ahamalov@users.noreply.github.com>
2025-02-14 22:52:58 +0000

    Update documentation

--- docs/html/classTrie.js
+++ docs/html/classTrie.js
@@ -9,5 +9,7 @@ var classTrie =
     [ "del", "classTrie.html#a286afcf0acdd84b189fe28016304cb00", null ],
     [ "getRoot", "classTrie.html#afc2a318e8bf33eac6117fac128c5d37c", null ],
     [ "insert", "classTrie.html#a82ded465e0c692fe9c783d471ed4f682", null ],
+    [ "operator+", "classTrie.html#a1028ddcc66a2c5c606269b4c8136da9e", null ],
+    [ "operator-", "classTrie.html#a22ccfa0315e46b7d05bc8e93b3139d74", null ],
     [ "operator=", "classTrie.html#acadfba10d18faea0b83d94a91548cd27", null ]
 ];
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['operator_3d_0',['operator=',['../classTrie.html#acadfba10d18faea0b83d94a91548cd27',1,'Trie']]]
+  ['operator_2b_0',['operator+',['../classTrie.html#a1028ddcc66a2c5c606269b4c8136da9e',1,'Trie']]],
+  ['operator_2d_1',['operator-',['../classTrie.html#a22ccfa0315e46b7d05bc8e93b3139d74',1,'Trie']]],
+  ['operator_3d_2',['operator=',['../classTrie.html#acadfba10d18faea0b83d94a91548cd27',1,'Trie']]]
 ];
