50289891b9ecebeeb61b0bf98adf5615a0245b8d
Arsenii-Ahamalov <Arsenii-Ahamalov@users.noreply.github.com>
2025-02-15 15:05:47 +0000

    Update documentation

--- docs/html/classTrie.js
+++ docs/html/classTrie.js
@@ -9,7 +9,13 @@ var classTrie =
     [ "del", "classTrie.html#a286afcf0acdd84b189fe28016304cb00", null ],
     [ "getRoot", "classTrie.html#afc2a318e8bf33eac6117fac128c5d37c", null ],
     [ "insert", "classTrie.html#a82ded465e0c692fe9c783d471ed4f682", null ],
+    [ "operator!=", "classTrie.html#a9bf6708fbfdd41349e7f3d9291ed286d", null ],
     [ "operator+", "classTrie.html#a1028ddcc66a2c5c606269b4c8136da9e", null ],
     [ "operator-", "classTrie.html#a22ccfa0315e46b7d05bc8e93b3139d74", null ],
-    [ "operator=", "classTrie.html#acadfba10d18faea0b83d94a91548cd27", null ]
+    [ "operator<", "classTrie.html#a6fd6312a05c467ef1a0729be5e8fdd6a", null ],
+    [ "operator<=", "classTrie.html#a7f417b706e75f60af3eeac0938f4cb57", null ],
+    [ "operator=", "classTrie.html#acadfba10d18faea0b83d94a91548cd27", null ],
+    [ "operator==", "classTrie.html#ae85a85f354ad4eed560129f59ef48535", null ],
+    [ "operator>", "classTrie.html#a2b5a5ae63e7c3439d942d7c1562dbfc2", null ],
+    [ "operator>=", "classTrie.html#abbd806b35f4ff8d76c3c8244d0b06741", null ]
 ];
--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -1,6 +1,12 @@
 var searchData=
 [
-  ['operator_2b_0',['operator+',['../classTrie.html#a1028ddcc66a2c5c606269b4c8136da9e',1,'Trie']]],
-  ['operator_2d_1',['operator-',['../classTrie.html#a22ccfa0315e46b7d05bc8e93b3139d74',1,'Trie']]],
-  ['operator_3d_2',['operator=',['../classTrie.html#acadfba10d18faea0b83d94a91548cd27',1,'Trie']]]
+  ['operator_21_3d_0',['operator!=',['../classTrie.html#a9bf6708fbfdd41349e7f3d9291ed286d',1,'Trie']]],
+  ['operator_2b_1',['operator+',['../classTrie.html#a1028ddcc66a2c5c606269b4c8136da9e',1,'Trie']]],
+  ['operator_2d_2',['operator-',['../classTrie.html#a22ccfa0315e46b7d05bc8e93b3139d74',1,'Trie']]],
+  ['operator_3c_3',['operator&lt;',['../classTrie.html#a6fd6312a05c467ef1a0729be5e8fdd6a',1,'Trie']]],
+  ['operator_3c_3d_4',['operator&lt;=',['../classTrie.html#a7f417b706e75f60af3eeac0938f4cb57',1,'Trie']]],
+  ['operator_3d_5',['operator=',['../classTrie.html#acadfba10d18faea0b83d94a91548cd27',1,'Trie']]],
+  ['operator_3d_3d_6',['operator==',['../classTrie.html#ae85a85f354ad4eed560129f59ef48535',1,'Trie']]],
+  ['operator_3e_7',['operator&gt;',['../classTrie.html#a2b5a5ae63e7c3439d942d7c1562dbfc2',1,'Trie']]],
+  ['operator_3e_3d_8',['operator&gt;=',['../classTrie.html#abbd806b35f4ff8d76c3c8244d0b06741',1,'Trie']]]
 ];
