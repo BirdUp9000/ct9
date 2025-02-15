0e264d881984fec193b1b7bb5e3bcab37077380f
Artem Romanov <53099138+BirdUp9000@users.noreply.github.com>
2025-02-16 00:28:08 +0100

    Develop to master. (#5)
    * GUI mode added
    
    * Added initial GUI/TUI modes.
    
    * Small fixes in CI pipeline and CMakeLists.txt. Added demo animation.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # CT9 - Prefix Tree Text Autocomplete
 CT9 is a project that implements a prefix tree (also known as a trie) data structure to provide text autocomplete functionality. The project utilizes the X11 library interface to display a text input field where users can interact with the autocomplete feature.
 
+## Demo animation
+![Demo animation](assets/demo_animation.webp)
+
+> [!TIP]
+>
+> The interface is intended only to demonstrate the library’s functionality. If you want to use this library in your own project, simply copy the `Trie.h` file.
+
 ## Features
 - Prefix tree (trie) data structure for efficient text autocomplete.
 - X11 library interface for displaying the text input field.
@@ -8,7 +15,10 @@ CT9 is a project that implements a prefix tree (also known as a trie) data struc
 - Ability to test and experiment with the autocomplete functionality.
 
 ## Usage
-To use the CT9 project, simply run the provided application. Users can then type into the text input field, and the application will display autocomplete suggestions based on the prefix of the user's input.
+To use the CT9 project, simply run the provided application. Users can then type into the text input field, and the application will display autocomplete suggestions based on the prefix of the user's input. <br>
+`Tab` – completes the current text. <br>
+`Return` – adds the typed word to the dictionary. <br>
+`Esc` – exits the program.
 
 ## Building
 ##### To build the CT9 project, follow these steps: <br>
@@ -20,6 +30,6 @@ Run the application <br>
 `./ct9`
 
 ## Requirements
-- C++17 or newer
+- C++20 or newer
 - CMake version 3.25 or higher
 - X11 development libraries: Make sure the libX11 library is installed.
--- src/main.cc
+++ src/main.cc
@@ -4,8 +4,185 @@
 
 #include "../include/ct9/Trie.h"
 
-int main() {
+#if BUILD_GUI
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#define POSX 100
+#define POSY 100
+#define WIDTH 400
+#define HEIGHT 50
+#define BORDER 1
+#define TEXTBOX_X 8
+#define TEXTBOX_Y 32
 
-  std::cout << "Hello, World" << "!\n";
-  return 0;
+#define TEXTBOX_MAX_CHARS 32
+
+static Display* dpy;
+static Window root;
+static int screen;
+#endif
+
+int main() {
+  Trie t{};
+  std::ifstream file("../tests/words.txt");
+
+  if (!file.is_open()) {
+    std::cerr << "File opening error.\n";
+    return EXIT_FAILURE;
+  }
+
+  std::string line;
+  while (std::getline(file, line)) {
+    t.insert(line);
+  }
+
+#if BUILD_GUI
+  // Creating window
+  dpy = XOpenDisplay(nullptr);
+  if (dpy == nullptr) {
+    std::cerr << "Failed to open X display" << '\n';
+    return EXIT_FAILURE;
+  }
+
+  screen = DefaultScreen(dpy);
+  root = RootWindow(dpy, screen);
+
+  // Colors
+  XColor color_border;
+  XColor color_background;
+  XColor color_suggestion;
+  XColor color_input;
+  XColor ignore;
+  // Border
+  XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "SlateBlue", &color_border, &ignore);
+  // Background
+  XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "MidnightBlue", &color_background, &ignore);
+  // Suggestion color
+  XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "DimGray", &color_suggestion, &ignore);
+  // User input text color
+  XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "White", &color_input, &ignore);
+
+  const Window win =
+      XCreateSimpleWindow(dpy, root, POSX, POSY, WIDTH, HEIGHT, BORDER, color_border.pixel, color_background.pixel);
+  XSelectInput(dpy, win, ExposureMask | KeyPressMask);
+  XMapWindow(dpy, win);
+
+  // Creating input box
+  GC gc = XCreateGC(dpy, win, 0, nullptr);
+  XSetForeground(dpy, gc, color_input.pixel);
+  std::string inputText;
+
+  // Choosing font
+  XFontStruct* fontInfo = XLoadQueryFont(dpy, "12x24");
+  if (fontInfo == nullptr) {
+    fontInfo = XLoadQueryFont(dpy, "9x15");
+    if (fontInfo == nullptr) {
+      fontInfo = XLoadQueryFont(dpy, "fixed");
+      if (fontInfo == nullptr) {
+        std::cerr << "No suitable font found! Exiting...\n";
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+  XSetFont(dpy, gc, fontInfo->fid);
+
+  // Result of autocompletion
+  std::queue<std::string> words;
+
+  bool running = true;
+  while (running) {
+    XEvent event;
+    XNextEvent(dpy, &event);
+
+    switch (event.type) {
+      case Expose:
+        XClearWindow(dpy, win);
+        XSetForeground(dpy, gc, color_input.pixel);
+        XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, inputText.c_str(), static_cast<int>(inputText.size()));
+        break;
+
+      case KeyPress: {
+        KeySym keysym;
+        char buffer[32];
+        int bytes = XLookupString(&event.xkey, buffer, sizeof(buffer), &keysym, nullptr);
+
+        if (keysym == XK_BackSpace) {
+          if (!inputText.empty()) {
+            inputText.pop_back();
+          }
+        } else if (keysym == XK_Escape) {
+          running = false;
+          break;
+        } else if (keysym == XK_Return) {
+          t.insert(inputText);
+          inputText.clear();
+        } else if (keysym == XK_Tab) {
+          auto result = t.autocomplete(inputText, 1);
+          if (!result.empty()) {
+            const auto& completed_str = result.front();
+            inputText = completed_str;
+            XSetForeground(dpy, gc, color_suggestion.pixel);
+            XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, completed_str.c_str(),
+                        static_cast<int>(completed_str.size()));
+          }
+        } else if (keysym == XK_space) {
+          break;
+        } else {
+          if (bytes > 0 && inputText.size() < TEXTBOX_MAX_CHARS) {
+            inputText.append(buffer, bytes);
+          }
+        }
+        XClearWindow(dpy, win);
+        auto result = t.autocomplete(inputText, 1);
+        if (!result.empty()) {
+          const auto& completed_str = result.front();
+          XSetForeground(dpy, gc, color_suggestion.pixel);
+          XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, completed_str.c_str(),
+                      static_cast<int>(completed_str.size()));
+        }
+
+        XSetForeground(dpy, gc, color_input.pixel);
+        XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, inputText.c_str(), static_cast<int>(inputText.size()));
+
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  if (fontInfo) {
+    XFreeFont(dpy, fontInfo);
+  }
+
+  XFreeGC(dpy, gc);
+  XUnmapWindow(dpy, win);
+  XDestroyWindow(dpy, win);
+  XCloseDisplay(dpy);
+
+#else
+#define MAX_SUGGESTIONS 5
+
+  while (true) {
+    std::cout << "Start writing a word and press Return:" << '\n';
+    std::string input;
+    std::getline(std::cin, input);
+
+    if (input == "quit") {
+      break;
+    }
+
+    std::cout << "Suggested words:" << '\n';
+    auto result = t.autocomplete(input, MAX_SUGGESTIONS);
+    while (!result.empty()) {
+      std::cout << result.front() << '\n';
+      result.pop();
+    }
+    std::cout << '\n';
+  }
+#endif
+
+  file.close();
+  return EXIT_SUCCESS;
 }
