ee758f0e540b9edfc9355a190be1e16e3963569a
BirdUp9000 <alexromanov322@gmail.com>
2025-02-15 17:25:16 +0100

    GUI mode added

--- src/main.cc
+++ src/main.cc
@@ -4,45 +4,155 @@
 
 #include "../include/ct9/Trie.h"
 
+#if BUILD_GUI
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#define POSX 100
+#define POSY 100
+#define WIDTH 200
+#define HEIGHT 50
+#define BORDER 1
+#define TEXTBOX_X 5
+#define TEXTBOX_Y 30
+
+static Display* dpy;
+static Window root;
+static int screen;
+#endif
+
 int main() {
-
-  std::cout << "Hello, World" << "!\n";
-
-
+  Trie t{};
   std::ifstream file("../tests/words.txt");
 
   if (!file.is_open()) {
-    std::cerr << "Failed to open file." << std::endl;
-    return 1;
+    std::cerr << "File opening error.\n";
+    return EXIT_FAILURE;
   }
 
   std::string line;
-  // Чтение файла построчно
   while (std::getline(file, line)) {
-    std::cout << line << std::endl;
+    t.insert(line);
   }
 
-  Trie t(line);
-  auto res = t.autocomplete("");
+  std::cout << t.autocomplete("").size() << std::endl;
 
-  //for (;;) {
-  //  if (res.empty()) break;
-  //  std::cout << res.front() << std::endl;
-  //  res.pop();
-  //}
-
-  Trie t2(std::move(t));
-
-  auto res2 = t2.autocomplete("");
-  for (;;) {
-    if (res2.empty()) break;
-    std::cout << res2.front() << std::endl;
-    res2.pop();
+#if BUILD_GUI
+  // Creating window
+  dpy = XOpenDisplay(nullptr);
+  if (dpy == nullptr) {
+    std::cerr << "Failed to open X display" << '\n';
+    return EXIT_FAILURE;
   }
 
-  t.insert("fuck");
+  screen = DefaultScreen(dpy);
+  root = RootWindow(dpy, screen);
 
-  t2 = t;
+  // Colors
+  XColor color_gray, color_blue, ignore;
+  XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "Web Gray", &color_gray, &ignore);
+  XAllocNamedColor(dpy, DefaultColormap(dpy, screen), "Light Blue", &color_blue, &ignore);
 
-  return 0;
+  const Window win =
+      XCreateSimpleWindow(dpy, root, POSX, POSY, WIDTH, HEIGHT, BORDER, BlackPixel(dpy, screen), color_blue.pixel);
+  XSelectInput(dpy, win, ExposureMask | KeyPressMask);
+  XMapWindow(dpy, win);
+
+  // Creating inputbox
+  GC gc = XCreateGC(dpy, win, 0, nullptr);
+  XSetForeground(dpy, gc, BlackPixel(dpy, screen));
+  std::string inputText;
+
+  // Choosing font
+  XFontStruct* fontInfo = XLoadQueryFont(dpy, "12x24");
+  if (fontInfo == nullptr) {
+    fontInfo = XLoadQueryFont(dpy, "9x15");
+    if (fontInfo == nullptr) {
+      fontInfo = XLoadQueryFont(dpy, "fixed");
+      if (fontInfo == nullptr) {
+        std::cerr << "No suitable font found! Exiting...\n";
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+  XSetFont(dpy, gc, fontInfo->fid);
+
+  // Result of autocompletion
+  std::queue<std::string> words;
+
+  bool running = true;
+  while (running) {
+    XEvent event;
+    XNextEvent(dpy, &event);
+
+    switch (event.type) {
+      case Expose:
+        XClearWindow(dpy, win);
+        XSetForeground(dpy, gc, BlackPixel(dpy, screen));
+        XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, inputText.c_str(), static_cast<int>(inputText.size()));
+        break;
+
+      case KeyPress: {
+        KeySym keysym;
+        char buffer[32];
+        int bytes = XLookupString(&event.xkey, buffer, sizeof(buffer), &keysym, nullptr);
+
+        if (keysym == XK_BackSpace) {
+          if (!inputText.empty()) {
+            inputText.pop_back();
+          }
+        } else if (keysym == XK_Escape) {
+          running = false;
+          break;
+        } else if (keysym == XK_Return) {
+          t.insert(inputText);
+          inputText.clear();
+        } else if (keysym == XK_Tab) {
+          auto result = t.autocomplete(inputText, 1);
+          if (!result.empty()) {
+            const auto& completed_str = result.front();
+            inputText = completed_str;
+            XSetForeground(dpy, gc, BlackPixel(dpy, screen));
+            XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, completed_str.c_str(), static_cast<int>(completed_str.size()));
+          }
+        } else if (keysym == XK_space) {
+          break;
+        } else {
+          if (bytes > 0) {
+            inputText.append(buffer, bytes);
+          }
+        }
+        XClearWindow(dpy, win);
+        auto result = t.autocomplete(inputText, 1);
+        if (!result.empty()) {
+          const auto& completed_str = result.front();
+          XSetForeground(dpy, gc, color_gray.pixel);
+          XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, completed_str.c_str(), static_cast<int>(completed_str.size()));
+        }
+
+        XSetForeground(dpy, gc, BlackPixel(dpy, screen));
+        XDrawString(dpy, win, gc, TEXTBOX_X, TEXTBOX_Y, inputText.c_str(), static_cast<int>(inputText.size()));
+
+        break;
+      }
+
+      default:
+        break;
+    }
+  }
+
+  if (fontInfo) {
+    XFreeFont(dpy, fontInfo);
+    fontInfo = nullptr;
+  }
+
+  XFreeGC(dpy, gc);
+  XUnmapWindow(dpy, win);
+  XDestroyWindow(dpy, win);
+  XCloseDisplay(dpy);
+#else
+  printf("Running without GUI.\n");
+#endif
+
+  file.close();
+  return EXIT_SUCCESS;
 }
