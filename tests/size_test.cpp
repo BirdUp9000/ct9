bbb345f23d113f80f6670ca0a3913f2a19c13fbc
Artem Romanov <53099138+BirdUp9000@users.noreply.github.com>
2025-02-16 13:41:45 +0100

    Added size method. (#6)

---
+++ tests/size_test.cpp
@@ -0,0 +1,63 @@
+// size_test.cpp
+
+#include <catch2/catch_all.hpp>
+#include <iostream>
+
+#include "../include/ct9/Trie.h"
+
+TEST_CASE("Size test on empty tree.") {
+  Trie t{};
+  REQUIRE(t.size() == 0);
+}
+
+TEST_CASE("Size test on trie with one word.") {
+  Trie t{};
+  t.insert("hello");
+  REQUIRE(t.size() == 5);
+}
+
+TEST_CASE("Size test on trie with many words.") {
+  Trie t{};
+  t.insert("hello");
+  t.insert("world");
+  REQUIRE(t.size() == 10);
+}
+
+TEST_CASE("Size test on trie after deletion.") {
+  Trie t{};
+  t.insert("hello");
+  t.delete("hello");
+  REQUIRE(t.size() == 0);
+}
+
+TEST_CASE("Size test on trie after copy.") {
+  Trie t1{};
+  t1.insert("hello");
+
+  Trie t2(t1);
+  REQUIRE(t2.size() == 5);
+}
+
+TEST_CASE("Size test on trie after assignment.") {
+  Trie t1{};
+  t1.insert("hello");
+
+  Trie t2 = t1;
+  REQUIRE(t2.size() == 5);
+}
+
+TEST_CASE("Size test on trie after move.") {
+  Trie t1{};
+  t1.insert("hello");
+
+  Trie t2(std::move(t1));
+  REQUIRE(t2.size() == 5);
+}
+
+
+
+
+
+
+
+
